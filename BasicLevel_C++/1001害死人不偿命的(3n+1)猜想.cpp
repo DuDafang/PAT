1fe656dc6a7affb94d19e7da6c87743e00feead5
yanglanqi <18821701664@163.com>
2019-02-05 12:49:15 +0800

    first commit

---
+++ BasicLevel_C++/1001害死人不偿命的(3n+1)猜想.cpp
@@ -0,0 +1,15 @@
+#include<cstdio>
+int main(){
+	int n;
+	scanf("%d",&n);
+	int count=0;
+	while(n!=1){
+		if(n%2==0){
+			n=n/2;
+		}
+		else n=(3*n+1)/2;
+		count++;
+	}
+	printf("%d",count); 
+	return 0;
+}
