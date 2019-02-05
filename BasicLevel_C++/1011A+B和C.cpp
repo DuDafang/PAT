954575383c2fbc1037e83f930095b71995985584
yanglanqi <18821701664@163.com>
2019-02-05 13:54:26 +0800

    -

---
+++ BasicLevel_C++/1011A+B和C.cpp
@@ -0,0 +1,13 @@
+#include<cstdio>
+using namespace std;
+int main(){
+	int n;
+	scanf("%d",&n);
+	long long a,b,c;
+	for(int i=1;i<=n;i++){
+		scanf("lld%lld%lld%",&a,&b,&c);
+		if(a+b>c) printf("Case #%d: true\n",i);
+		else printf("Case #%d: false\n",i);
+	}
+	return 0;
+}
